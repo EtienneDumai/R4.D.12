6d872ea17907ee1f2bd0a65ef6324903f3ac93fe
Etienne Dumai <etienne.dumai@gmail.com>
2025-02-19 15:38:12 +0100

    feat(component, apparence): changement dans l'affichage des cd avec detail du cd

--- cdStore/src/app/cd/cd.component.ts
+++ cdStore/src/app/cd/cd.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CD } from '../models/cd.model';
+import { CdsService } from '../cds.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cd',
   templateUrl: './cd.component.html',
   styleUrl: './cd.component.scss',
 })
-export class CDComponent {
+export class CDComponent implements OnInit {
   @Input() Cd!: CD;
+  theCd!: CD;
+  idcd!: string;
+  constructor(private cdService : CdsService, private route : ActivatedRoute) {}
+    ngOnInit(): void {
+      this.idcd = this.route.snapshot.params['id'];
+      if(this.idcd!==undefined){
+        this.theCd = this.cdService.getCdById(+this.idcd);
+      }
+      else{
+        this.theCd = this.Cd;
+      }
+    }
+  
   public  onAddCD(): void {
     this.Cd.quantite++;
   }
-  
 }
